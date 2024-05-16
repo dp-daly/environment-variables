0e09a0fd66ed81afdba5139501dfac57a0ae6b49
dp-daly <dan.daly@education.gov.uk>
2024-05-16 10:04:55 +0100

    backup

---
+++ models/books.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    year: { type: Number, required: true },
+    publisher: {type: String, required: true},
+    language: { type: String, required: true },
+    country: { type: String, required: true },
+    genre: { type: String, required: false},
+    details: { type: String, required: false},
+});
+
+module.exports = mongoose.model('Book', bookSchema);
---
+++ seed.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const books = require('./models/books.js')
+
+async function seed() {
+    console.log('Seeding has begun 🌱')
+    mongoose.connect(process.env.MONGODB_URI)
+    console.log('Connection is successful 🚀')
+
+    const book = await books.create({
+        title: "Journey from the North",
+        author: "Storm Jameson",
+        year: 1969,
+        publisher: "Pushkin Press",
+        language: "English",
+        country: "United Kingdom",
+        genre: "Memoir",
+    },
+
+    {
+        title: "In Search of Lost Time, Vol 1: Swann's Way",
+        author: "Marcel Proust",
+        year: 1913,
+        publisher: "Gallimard",
+        language: "French",
+        country: "France",
+        genre: "Novel",
+    },
+
+    {
+        title: "The Lodgers",
+        author: "Holly Pester",
+        year: 2024, 
+        publisher: "Granta Books",
+        language: "English",
+        country: "United Kingdom",
+        genre: "Novel",
+        details: "Debut novel by an award-winning poet."
+    }
+)
+
+    console.log(book);
+
+}
+
+seed();
